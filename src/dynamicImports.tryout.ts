63854e6323a149821baaf4141b30ad49351bc690
Chintu Kishen KM <me.chintukm@gmail.com>
2019-07-11 12:39:57 +0200

    ...

--- src/dynamicImports.tryout.ts
+++ src/dynamicImports.tryout.ts
@@ -1,5 +1,5 @@
 export const dynamicImports = () => {
-  import('./math').then(math => {
+  import('./utils/math').then(math => {
     console.log('result => ', math.add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
   });
 
