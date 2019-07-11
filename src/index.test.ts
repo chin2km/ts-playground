bc5ad5467673acd73837d30643320013b6ef0ed5
Chintu Kishen KM <me.chintukm@gmail.com>
2019-07-11 11:50:21 +0200

    ...

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,25 +1,5 @@
-import { BoardGame } from "./index";
-
-describe("A popular game",() => {
-  const gameInstance =new BoardGame(6,3);
-
-  it("creates initial board", () => {
-    expect(gameInstance.board).toMatchSnapshot("Initial");
-  })
-  it("after selecting 💛", () => {
-    gameInstance.select(`💛`);
-    expect(gameInstance.board).toMatchSnapshot("after selecting 💛");
-  })
-  it("after selecting ❤️", () => {
-    gameInstance.select(`❤️`);
-    expect(gameInstance.board).toMatchSnapshot("after selecting ❤️");
-  })
-  it("after selecting 💜", () => {
-    gameInstance.select(`💜`);
-    expect(gameInstance.board).toMatchSnapshot("after selecting 💜");
-  })
-  it("after selecting 💚", () => {
-    gameInstance.select(`💚`);
-    expect(gameInstance.board).toMatchSnapshot("after selecting 💚");
-  })
-})
+describe('Test', () => {
+  it('scenario', () => {
+    expect(1).toMatchSnapshot('snap');
+  });
+});
